95a80dc7cea5e08c7c13d75d3d02da9179ad4c3e
ELIUS WANYORO <109586789+EliusCaleb@users.noreply.github.com>
2024-01-09 11:36:34 +0300

    adding fullcalenda to app

---
+++ src/components/Calendar.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import interactionPlugin from '@fullcalendar/interaction'
+
+
+const Calendar = () => {
+
+    const handleDateSelect = (selectInfo) =>{
+        let title = prompt('enter a valid title')
+        let calendarApi = selectInfo.view.calendar 
+        calendarApi.unselect()
+
+        if(title){
+           calendarApi.addEvent({
+             title,
+             start:selectInfo.startStr,
+             end:selectInfo.endStr
+           }) 
+
+
+           fetch('api/events',{
+            method: 'Post',
+            headers:  {
+                'Content-Type': 'application/json'
+            },
+            body:JSON.stringify({
+                title: title,
+                start:selectInfo.startStr,
+                end:selectInfo.endStr
+            })
+           })
+        }
+
+    }
+  return (
+    <div>
+        <FullCalendar
+          initialViews ='timeGridDay'
+          headerToolbar ={
+            {
+                left: 'prev,next, today addEventButton',
+                center:'title',
+                right:'dayGridMonth, timeGridWeek,timeGridDay'
+            }
+          }
+          plugins={[dayGridPlugin,timeGridPlugin,interactionPlugin]}
+          editable={true}
+          selectable={true}
+          select={handleDateSelect}
+          events ={
+            {
+                url: '',
+                color: 'white',
+                textColor: 'black'
+            }
+          }
+        />
+    </div>
+  )
+}
+
+export default Calendar
